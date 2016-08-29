62c5e8a807436c5062fe8c5cb011c46922573a79
Simon Dollé <simon.dolle@gmail.com>
2016-08-29 14:04:12 +0200

    Add completion to other commands

--- hlscomplete_autocomplete.sh
+++ hlscomplete_autocomplete.sh
@@ -9,3 +9,8 @@ _script()
   return 0
 }
 complete -o nospace -F _script hls
+complete -o nospace -F _script hcat
+complete -o nospace -F _script hrm
+complete -o nospace -F _script hdu
+complete -o nospace -F _script simprot
+complete -o nospace -F _script richprot
