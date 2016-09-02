e3f769dc4eadc22cb9aa2394bf00ccd09e585bdf
Simon Dollé <simon.dolle@gmail.com>
2016-09-02 18:56:01 +0200

    Add some unit tests

--- tests/test_get_completions.py
+++ tests/test_get_completions.py
@@ -290,7 +290,15 @@ class TestLsParser(unittest.TestCase):
         self.assertEquals(update_completions.FileStatus("/Users/simon/Personal Documents", True), parser.parse_line(line))
 
 
-
+class TestSplitPath(unittest.TestCase):
+    def test_nominal_case(self):
+        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music")
+        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music/")
+        
+class TestAppendSlash(unittest.TestCase):
+    def test_nominal_case(self):
+        self.assertEquals("user/s.dolle/Music/", get_completions.append_slash("/user/s.dolle/Music", True)
+        self.assertEquals("user/s.dolle/Music", get_completions.append_slash("/user/s.dolle/Music", False)
 
 
 if __name__ == '__main__':
