9088517279904028192c561e226507b18e983d58
Simon Dollé <simon.dolle@gmail.com>
2016-09-02 18:58:47 +0200

    Close parentheses

--- tests/test_get_completions.py
+++ tests/test_get_completions.py
@@ -292,13 +292,13 @@ class TestLsParser(unittest.TestCase):
 
 class TestSplitPath(unittest.TestCase):
     def test_nominal_case(self):
-        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music")
-        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music/")
+        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music"))
+        self.assertEquals(["user", "s.dolle", "Music"], get_completions.split_path("/user/s.dolle/Music/"))
         
 class TestAppendSlash(unittest.TestCase):
     def test_nominal_case(self):
-        self.assertEquals("user/s.dolle/Music/", get_completions.append_slash("/user/s.dolle/Music", True)
-        self.assertEquals("user/s.dolle/Music", get_completions.append_slash("/user/s.dolle/Music", False)
+        self.assertEquals("user/s.dolle/Music/", get_completions.append_slash("/user/s.dolle/Music", True))
+        self.assertEquals("user/s.dolle/Music", get_completions.append_slash("/user/s.dolle/Music", False))
 
 
 if __name__ == '__main__':
