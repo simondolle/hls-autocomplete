117cf4302d6989eb06d06072d0ff9d0d5be2c710
Simon Dollé <simon.dolle@gmail.com>
2016-09-02 18:14:22 +0200

    Create hlscomplete_autocomplete.sh

---
+++ hlscomplete_autocomplete.sh
@@ -0,0 +1,16 @@
+_script()
+{ 
+  local cur
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  HLS_COMPLETE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  _script_commands=$(python $HLS_COMPLETE_DIR/hls_complete/get_completions.py $cur)
+  COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+  return 0
+}
+complete -o nospace -F _script hls
+complete -o nospace -F _script hcat
+complete -o nospace -F _script hrm
+complete -o nospace -F _script hdu
+complete -o nospace -F _script simprot
+complete -o nospace -F _script richprot
