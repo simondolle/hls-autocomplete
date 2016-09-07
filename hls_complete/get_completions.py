bf522f056ad9d29a3cabcb5b6ad38162f7cc53eb
Simon Dollé <simon@botify.com>
2016-09-07 22:04:55 +0200

    create utils.py

--- hls_complete/get_completions.py
+++ hls_complete/get_completions.py
@@ -4,28 +4,7 @@ import os.path
 import json
 import sys
 
-def get_cache_path():
-    cache_dir = os.path.expanduser("~")
-    input_file = os.path.join(cache_dir, ".hls_cache.json")
-    return input_file
-
-def load_cache():
-    input_file = get_cache_path()
-    if not os.path.exists(input_file):
-        return {}
-    else:
-        return json.load(open(input_file))
-
-def split_path(path):
-    path_chunks = path.split("/")
-    path_chunks = [chunk for chunk in path_chunks if len(chunk) != 0]
-    return path_chunks
-
-def append_slash(path, is_dir):
-    if is_dir:
-        return path + "/"
-    else:
-        return path
+from hls_complete.utils import append_slash, split_path, load_cache
 
 def get_completions(path, cache):
     dirname = os.path.dirname(path)
