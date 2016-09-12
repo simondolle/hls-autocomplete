6620b5aa7287c4737f4f2cadbfa2d2f271ba4c86
Simon Dollé <simon@botify.com>
2016-09-12 22:26:31 +0200

    Create CacheCompleter

--- hls_autocomplete/complete.py
+++ hls_autocomplete/complete.py
@@ -13,42 +13,47 @@ def get_path_to_complete(path):
         result = os.path.dirname(path)
     return result
 
-def get_completions(path, cache):
-    dirname = get_path_to_complete(path)
-    path_chunks = split_path(dirname)
-    for path_chunk in path_chunks:
-        if path_chunk not in cache:
+class CacheCompleter(object):
+    def __init__(self):
+        pass
+
+    def get_completions(self, path, cache):
+        dirname = get_path_to_complete(path)
+        path_chunks = split_path(dirname)
+        for path_chunk in path_chunks:
+            if path_chunk not in cache:
+                return []
+            cache = cache[path_chunk]
+        if not type(cache) == type(dict()):
             return []
-        cache = cache[path_chunk]
-    if not type(cache) == type(dict()):
-        return []
-    result = []
-    for key, value in cache.items():
-        result.append((key, value is not None))
+        result = []
+        for key, value in cache.items():
+            result.append((key, value is not None))
 
-    if not path.endswith("/"):
-        partial_basename = os.path.basename(path)
-        result = [s for s in result if s[0].startswith(partial_basename)]
+        if not path.endswith("/"):
+            partial_basename = os.path.basename(path)
+            result = [s for s in result if s[0].startswith(partial_basename)]
 
-    if len(path_chunks) == 0:
-        result = [("/" + r[0], r[1]) for r in result]
-    result = [append_slash(r[0], r[1]) for r in result]
-    result = [os.path.join(dirname, r) for r in result]
-    return sorted(result)
+        if len(path_chunks) == 0:
+            result = [("/" + r[0], r[1]) for r in result]
+        result = [append_slash(r[0], r[1]) for r in result]
+        result = [os.path.join(dirname, r) for r in result]
+        return sorted(result)
 
-def get_completions_with_update(path, cache, lister):
-    completions = get_completions(path, cache)
-    if len(completions) == 0:
-        _, cache = lister.hls_with_update(get_path_to_complete(path))
-        completions = get_completions(path, cache)
-    return completions
+    def get_completions_with_update(self, path, cache, lister):
+        completions = self.get_completions(path, cache)
+        if len(completions) == 0:
+            _, cache = lister.hls_with_update(get_path_to_complete(path))
+            completions = self.get_completions(path, cache)
+        return completions
 
 def main():
     hls_cache = load_cache()
     if len(sys.argv) > 1:
         input_path = sys.argv[1].decode("utf-8")
         lister = HlsHdfs()
-        completions = get_completions_with_update(input_path, hls_cache, lister)
+        completer = CacheCompleter()
+        completions = completer.get_completions_with_update(input_path, hls_cache, lister)
         completions = ["%s"%s for s in completions]
         result = "\n".join(completions)
         print result.encode("utf-8")
--- tests/test_complete.py
+++ tests/test_complete.py
@@ -1,7 +1,7 @@
 import unittest
 import mock
 
-from hls_autocomplete.complete import get_completions, get_path_to_complete, get_completions_with_update
+from hls_autocomplete.complete import CacheCompleter, get_path_to_complete
 
 class TestGetCompletions(unittest.TestCase):
 
@@ -21,69 +21,73 @@ class TestGetCompletions(unittest.TestCase):
                 }
             }
         }
+        self.completer = CacheCompleter()
 
 
     def test_nominal_case(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/simon/D", self.json)
         expected_result = ["/Users/simon/Documents/", "/Users/simon/Dropbox/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_directory_case(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/simon/", self.json)
         expected_result = ["/Users/simon/Documents/", "/Users/simon/Dropbox/", "/Users/simon/Music/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_directory_case(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/simon/", self.json)
         expected_result = ["/Users/simon/Documents/", "/Users/simon/Dropbox/", "/Users/simon/Music/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_unexisting_directory(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/toto/t", self.json)
         expected_result = []
         self.assertEquals(expected_result, actual_result)
 
     def test_too_long_path(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/simon/Documents/work/old", self.json)
         expected_result = []
         self.assertEquals(expected_result, actual_result)
 
     def test_double_slash(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
             "/Users/simon//", self.json)
         expected_result = ["/Users/simon/Documents/", "/Users/simon/Dropbox/", "/Users/simon/Music/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_root_only(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/", self.json)
         expected_result = ["/Users/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_second_level(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/si", self.json)
         expected_result = ["/Users/simon/"]
         self.assertEquals(expected_result, actual_result)
 
     def test_file_with_extension(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users/simon/Documents/work/C", self.json)
         expected_result = ["/Users/simon/Documents/work/CV.doc"]
         self.assertEquals(expected_result, actual_result)
 
     def test_empty_json(self):
-        actual_result = get_completions(
+        actual_result = self.completer.get_completions(
                 "/Users", {})
         expected_result = []
         self.assertEquals(expected_result, actual_result)
 
 class TestGetCompletionsWithUpdate(unittest.TestCase):
+    def setUp(self):
+        self.completer = CacheCompleter()
+
     def testNoUpdate(self):
         cache = {
             "Users": {
@@ -102,7 +106,7 @@ class TestGetCompletionsWithUpdate(unittest.TestCase):
         }
         lister_mock = mock.MagicMock()
         path = "/Users/s"
-        self.assertEquals(["/Users/simon/"], get_completions_with_update(path, cache, lister_mock))
+        self.assertEquals(["/Users/simon/"], self.completer.get_completions_with_update(path, cache, lister_mock))
         lister_mock.hls_with_update_mock.assert_not_called()
 
     def testUpdate(self):
@@ -125,7 +129,7 @@ class TestGetCompletionsWithUpdate(unittest.TestCase):
         lister_mock = mock.MagicMock()
         lister_mock.hls_with_update.return_value = (None, updated_cache)
         self.assertEquals(["/Users/simon/Documents/", "/Users/simon/Dropbox/"],
-                          get_completions_with_update(path, cache, lister_mock))
+                          self.completer.get_completions_with_update(path, cache, lister_mock))
 
 
 class TestGetPathToComplete(unittest.TestCase):
