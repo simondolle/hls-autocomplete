d8e0a4a407b26992713a9fa462e54f7169412ed2
Simon Dollé <simon@botify.com>
2016-09-25 22:04:41 +0200

    remove more caching logic

--- hls_autocomplete/hls.py
+++ hls_autocomplete/hls.py
@@ -3,22 +3,6 @@ import os.path
 
 from update import LsParser
 
-class CacheHls(object):
-    def __init__(self, lister, cache):
-        self.lister = lister
-        self.cache = cache
-
-    def list_status(self, path):
-        hls_return_code, hls_result = self.lister.hls(path)
-        if hls_return_code == 0:
-            self.update_cache(path, hls_result)
-        return hls_result
-
-    def update_cache(self, path, hls_result):
-
-        self.cache = self.cache.update_directory(path, hls_result)
-        return self.cache
-
 class HlsSubprocess(object):
     def list_status(self, path):
         return self.hls(path)
